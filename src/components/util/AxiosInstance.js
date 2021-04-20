ac54d220c7e8fed2c855e1ca583344a77d51a039
mprkatti <praskatti@yahoo.com>
2021-04-20 12:43:18 +0530

    Changed API end point

--- src/components/util/AxiosInstance.js
+++ src/components/util/AxiosInstance.js
@@ -5,7 +5,7 @@ axios.defaults.headers.common['Content-Type'] = 'application/json';
 axios.defaults.headers.common['Accept'] = 'application/json';
 
 export const AxiosInstance = axios.create({
-  baseURL: "https://sample-nodejs-deploy.herokuapp.com",
+  baseURL: "https://sample-nodejs-depl-2.herokuapp.com",
   timeout: 5000
 });
 
